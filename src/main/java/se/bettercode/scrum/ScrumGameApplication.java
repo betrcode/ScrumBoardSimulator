ee90b1a1a264d074ef63883cc02465484cbd5300
Max Wenzin <max.wenzin@gmail.com>
2014-09-13 21:06:54 +0200

    Added buttons and events

--- src/main/java/se/bettercode/scrum/ScrumGameApplication.java
+++ src/main/java/se/bettercode/scrum/ScrumGameApplication.java
@@ -1,15 +1,19 @@
-package se.bettercode.scrum;/**
- * Created by max on 9/13/14.
- */
+package se.bettercode.scrum;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 
 public class ScrumGameApplication extends Application {
+
+    Label response;
 
     public static void main(String[] args) {
         System.out.println("Launching JavaFX application.");
@@ -26,12 +30,31 @@ public class ScrumGameApplication extends Application {
 
         System.out.println("Inside start()");
         primaryStage.setTitle("Scrum Game");
-        FlowPane rootNode = new FlowPane();
+        FlowPane rootNode = new FlowPane(10, 10);
+        rootNode.setAlignment(Pos.CENTER);
         Scene myScene = new Scene(rootNode, 300, 200);
         primaryStage.setScene(myScene);
 
-        Label myLabel = new Label("This is the Scrum Game");
-        rootNode.getChildren().add(myLabel);
+        response = new Label("Push a button");
+
+        Button btnAlpha = new Button("Alpha");
+        Button btnBeta = new Button("Beta");
+
+        btnAlpha.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                response.setText("Alpha was pressed.");
+            }
+        });
+
+        btnBeta.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                response.setText("Beta was pressed.");
+            }
+        });
+
+        rootNode.getChildren().addAll(btnAlpha, btnBeta, response);
 
         primaryStage.show();
 
