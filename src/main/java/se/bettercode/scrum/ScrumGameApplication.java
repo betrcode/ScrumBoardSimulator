b5c715904e6c1b68672e573702d192dc54f244ee
Max Wenzin <max.wenzin@gmail.com>
2014-10-05 14:33:15 +0200

    Added ChoiceBox to select between different backlogs

--- src/main/java/se/bettercode/scrum/ScrumGameApplication.java
+++ src/main/java/se/bettercode/scrum/ScrumGameApplication.java
@@ -1,15 +1,21 @@
 package se.bettercode.scrum;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import se.bettercode.scrum.backlog.Backlog;
+import se.bettercode.scrum.backlog.SmallBacklog;
 import se.bettercode.scrum.backlog.WellSlicedBacklog;
 import se.bettercode.scrum.gui.Board;
 import se.bettercode.scrum.gui.StatusBar;
 import se.bettercode.scrum.gui.ToolBar;
 import se.bettercode.scrum.prefs.StageUserPrefs;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ScrumGameApplication extends Application {
@@ -19,7 +25,9 @@ public class ScrumGameApplication extends Application {
     private Team team;
     private Backlog backlog;
     private StatusBar statusBar = new StatusBar();
-    private ToolBar toolBar = new ToolBar();
+    //private Map<String, Backlog> backlogMap = new HashMap<>();
+    private String[] backlogNames = new String[]{"Small", "Well sliced"};
+    private ToolBar toolBar = new ToolBar(backlogNames);
     private Stage primaryStage;
     private StageUserPrefs prefs;
 
@@ -54,9 +62,18 @@ public class ScrumGameApplication extends Application {
         primaryStage.setScene(new Scene(borderPane, 800, 600));
     }
 
-    private void initSprint() {
+    private void initSprint(String backlogName) {
         team = new Team("The Cobras", 23);
-        backlog = new WellSlicedBacklog();
+
+        switch (backlogName) {
+            case "Small":
+                backlog = new SmallBacklog();
+                break;
+            case "Well sliced":
+                backlog = new WellSlicedBacklog();
+                break;
+        }
+
         sprint = new Sprint("First sprint", 10, team, backlog);
 
         board.bindBacklog(backlog);
@@ -73,12 +90,20 @@ public class ScrumGameApplication extends Application {
     }
 
     private void bindActionsToToolBar() {
-        toolBar.setLoadButtonAction(event -> loadData());
+        ChangeListener changeListener = new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                loadData(newValue.toString());
+            }
+        };
+
+        toolBar.setChangeListener(changeListener);
+        toolBar.setLoadButtonAction(event -> loadData("Small"));
         toolBar.setStartButtonAction((event) -> sprint.runSprint());
     }
 
-    private void loadData() {
-        initSprint();
+    private void loadData(String backlogName) {
+        initSprint(backlogName);
         bindSprintDataToStatusBar();
     }
 
--- src/main/java/se/bettercode/scrum/gui/ToolBar.java
+++ src/main/java/se/bettercode/scrum/gui/ToolBar.java
@@ -1,26 +1,38 @@
 package se.bettercode.scrum.gui;
 
 import javafx.beans.property.BooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.HBox;
 
 public class ToolBar extends HBox {
 
     private final Button loadButton = new Button("Load board");
     private final Button startButton = new Button("Start Sprint");
+    private ChoiceBox choiceBox = new ChoiceBox();
 
-    public ToolBar() {
+    public void setChangeListener(ChangeListener changeListener) {
+        choiceBox.getSelectionModel().selectedItemProperty().addListener(changeListener);
+    }
+
+    public ToolBar(String[] backlogs) {
         setPadding(new Insets(15, 12, 15, 12));
         setSpacing(10);
         setStyle("-fx-background-color: #336699;");
 
+        choiceBox.setItems(FXCollections.observableArrayList(backlogs));
+        choiceBox.setTooltip(new Tooltip("Select backlog"));
         loadButton.setPrefSize(100, 20);
         startButton.setPrefSize(100, 20);
 
-        getChildren().addAll(loadButton, startButton);
+        getChildren().addAll(choiceBox, loadButton, startButton);
     }
 
     public void setLoadButtonAction(EventHandler<ActionEvent> eventHandler) {
@@ -35,4 +47,5 @@ public class ToolBar extends HBox {
         loadButton.disableProperty().bind(booleanProperty);
         startButton.disableProperty().bind(booleanProperty);
     }
+
 }
