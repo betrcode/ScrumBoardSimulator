b3560a66671d6493516fb9f2420af2065f186bf1
Max Wenzin <max.wenzin@gmail.com>
2014-10-06 21:06:42 +0200

    Added WIP limit on Team, but not using it yet.

--- src/main/java/se/bettercode/scrum/team/Team.java
+++ src/main/java/se/bettercode/scrum/team/Team.java
@@ -10,9 +10,17 @@ public class Team {
     private StringProperty name;
     private IntegerProperty velocity;
 
+    private IntegerProperty workInProgressLimit;
+
     public Team(String name, int velocity) {
+        this(name, velocity, 1);
+    }
+
+    public Team(String name, int velocity, int workInProgressLimit) {
+        assert workInProgressLimit >= 1: "WIP limit must be larger than 0";
         this.name = new SimpleStringProperty(name);
         this.velocity = new SimpleIntegerProperty(velocity);
+        this.workInProgressLimit = new SimpleIntegerProperty(workInProgressLimit);
     }
 
     public final StringProperty nameProperty() {
@@ -35,11 +43,20 @@ public class Team {
         this.velocity.set(velocity);
     }
 
+    public int getWorkInProgressLimit() {
+        return workInProgressLimit.get();
+    }
+
+    public IntegerProperty workInProgressLimitProperty() {
+        return workInProgressLimit;
+    }
+
     @Override
     public String toString() {
         return "Team{" +
                 "name=" + name.getValue() +
                 ", velocity=" + velocity.get() +
+                ", WIP limit=" + workInProgressLimit.get() +
                 '}';
     }
 }
--- src/test/java/se/bettercode/scrum/TeamTest.java
+++ src/test/java/se/bettercode/scrum/TeamTest.java
@@ -7,7 +7,23 @@ public class TeamTest extends TestCase {
 
     public void testToString() {
         Team team = new Team("Dingos", 17);
-        assertEquals("Team{name=Dingos, velocity=17}", team.toString());
+        assertEquals("Team{name=Dingos, velocity=17, WIP limit=1}", team.toString());
+    }
 
+    public void testDefaultVelocity() {
+        Team team = new Team("Rhinos", 10);
+        assertEquals(1, team.getWorkInProgressLimit());
+    }
+
+    public void testInvalidWIPLimit() {
+        Throwable e = null;
+
+        try {
+            Team team = new Team("Monkeys", 20, 0);
+        } catch (Throwable ex) {
+            e = ex;
+        }
+
+        assertTrue(e instanceof AssertionError);
     }
 }
